e2dc69238a41a4b7adf33270ae54e7e25f526ee0
Ustin <ustinx@mail.ru>
2018-12-23 00:15:42 +0300

    app data commit

---
+++ Apartments/App_Data/sql_queries/ApartCreate.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Apartments]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Author] NVARCHAR(50) NOT NULL,
+	[Price] NVARCHAR(50) NOT NULL,
+	[Phone] INT NULL,
+	[Description] NVARCHAR(1500) NULL,
+	[PhoneImg] varbinary(max) NULL,
+	[MainApPhoto] varbinary(max) NULL,
+	[DateCreated] datetime NOT NULL,
+	[DateActualTo] datetime NOT NULL,
+	[IsActive] BIT NOT NULL,
+	[IsDonated] BIT NOT NULL,
+	[DonateDueDate] datetime NULL,
+	[InternalComment] NVARCHAR(2000) NULL,
+	[ClientId] INT NULL,
+	CONSTRAINT [ApartmentClients] FOREIGN KEY ([ClientId]) REFERENCES [Clients]([Id]) ON DELETE SET NULL
+)
---
+++ Apartments/App_Data/sql_queries/ClientsCreate.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Clients]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Surname] NVARCHAR(50) NULL,
+	[Fathername] NVARCHAR(50) NULL,
+	[Address] NVARCHAR(50) NULL,
+	[Agency] NVARCHAR(50) NULL,
+	[Internalcomment] NVARCHAR(50) NULL,
+	[Phone] INT NULL,
+	[BirthDate] datetime NOT NULL,
+	[RegistrationDate] datetime NOT NULL,
+	[Isagent] BIT NOT NULL,
+	[Isdonated] BIT NOT NULL,
+	[AvatarId] INT NULL
+)
