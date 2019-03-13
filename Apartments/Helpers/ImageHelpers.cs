21471368795979e4dfed87f57bca75300fc80f0a
Ustin <ustinx@mail.ru>
2019-03-13 23:40:46 +0300

    shows images on details page

---
+++ Apartments/Helpers/ImageHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+
+namespace Apartments.Helpers
+{
+    public static class ImageHelpers
+    {
+        public static string GetImageUrl(string shortId, string fileName)
+        {
+            string hardcodedUrl = "~/Images/kvartirant/";
+
+            return $"{hardcodedUrl}{shortId}" + "/" + fileName;
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string alt)
+        {
+            TagBuilder img = new TagBuilder("img");
+            img.MergeAttribute("src", src);
+            img.MergeAttribute("alt", alt);
+
+            return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper, string shortId, string fileName, string alt)
+        {
+            TagBuilder img = new TagBuilder("img");
+            img.MergeAttribute("src", GetImageUrl(shortId, fileName));
+            img.MergeAttribute("alt", alt);
+
+            return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper, string path, string shortId, string fileName, string alt)
+        {
+            string fullPath = path + shortId + "/" + fileName;
+
+            TagBuilder img = new TagBuilder("img");
+            img.MergeAttribute("src", fullPath);
+            img.MergeAttribute("alt", alt);
+
+            return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
